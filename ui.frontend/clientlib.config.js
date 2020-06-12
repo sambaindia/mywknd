c626946090e8e6ef43056ab12c91cd3fe8befd64
sbethamc <sbethamc@S34282.local>
2020-06-12 18:10:09 -0400

    First commit

---
+++ ui.frontend/clientlib.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+    // default working directory (can be changed per 'cwd' in every asset option)
+    context: __dirname,
+
+    // path to the clientlib root folder (output)
+    clientLibRoot: "./../ui.apps/src/main/content/jcr_root/apps/mywknd/clientlibs",
+
+    libs: [
+        {
+            name: "clientlib-dependencies",
+            allowProxy: true,
+            categories: ["mywknd.dependencies"],
+            serializationFormat: "xml",
+            cssProcessor : ["default:none", "min:none"],
+            jsProcessor: ["default:none", "min:none"],
+            assets: {
+                js: [
+                    "dist/clientlib-dependencies/*.js",
+                ],
+                css: [
+                    "dist/clientlib-dependencies/*.css"
+                ]
+            }
+        },
+        {
+            name: "clientlib-site",
+            allowProxy: true,
+            categories: ["mywknd.site"],
+            dependencies: ["mywknd.dependencies"],
+            serializationFormat: "xml",
+            cssProcessor : ["default:none", "min:none"],
+            jsProcessor: ["default:none", "min:none"],
+            assets: {
+                js: [
+                    "dist/clientlib-site/*.js",
+                ],
+                css: [
+                    "dist/clientlib-site/*.css"
+                ],
+                resources: [
+                    {src: "dist/clientlib-site/resources/images/*.*", dest: "images/"}, 
+                    {src: "dist/clientlib-site/resources/fonts/*.*", dest: "fonts/"}, 
+                ]
+            }
+        }
+    ]
+};
